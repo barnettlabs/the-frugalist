67c63c0e4d28ce1b9415b4cb4594351609b378a3
Jason Barnett <jbarnett2114@gmail.com>
2026-02-01 11:39:06 -0500

    add black logo and swap fox icon in blade email template

--- api/resources/views/vendor/mail/html/header.blade.php
+++ api/resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ url('images/fox-logo.png') }}" class="logo" alt="{{ trim($slot) }} Logo" style="height: 50px; max-height: 50px; width: 50px;">
+<img src="{{ url('images/logo-black.png') }}" class="logo" alt="{{ trim($slot) }} Logo" style="height: 50px; max-height: 50px; width: 50px;">
 </a>
 </td>
 </tr>
